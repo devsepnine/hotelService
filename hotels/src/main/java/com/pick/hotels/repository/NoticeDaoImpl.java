9a6a1acfa893dac1cda1487ceab669d137025ee8
kimnayeoni <50163350+kimnayeoni@users.noreply.github.com>
2019-08-20 10:11:16 +0900

    Add comment to NoticeController, NoticeDaoImpl

--- hotels/src/main/java/com/pick/hotels/repository/NoticeDaoImpl.java
+++ hotels/src/main/java/com/pick/hotels/repository/NoticeDaoImpl.java
@@ -16,37 +16,41 @@ public class NoticeDaoImpl implements NoticeDao{
 	@Autowired
 	private SqlSession sqlSession;
 	
+	//글쓰기
 	@Override
 	public void write(NoticeDto noticeDto) {
 		sqlSession.insert("notice.write", noticeDto);
-		
 	}
 
+	//글정보 가져오기
 	@Override
 	public NoticeDto get(int no) {
 		return sqlSession.selectOne("notice.get", no);
 	}
 
+	//글 삭제
 	@Override
 	public void delete(int no) {
 		sqlSession.delete("notice.delete", no);
-		
 	}
 
+	//글 목록
 	@Override
 	public List<NoticeDto> list(String type, String keyword) {
 		return sqlSession.selectList("notice.list");
 	}
 
+	//글 조회수
 	@Override
 	public void read(int no) {
 		sqlSession.update("notice.read", no);
-		
 	}
 
+	//글 목록/검색일때 각각의 게시글 수 구하기
 	@Override
 	public int count(String type, String keyword) {
 		Map<String, String> param = new HashMap<>();
+		
 		if(type != null && keyword != null) {
 			param.put("type", type.replace("+", "||"));
 			param.put("keyword", keyword);
@@ -54,15 +58,16 @@ public class NoticeDaoImpl implements NoticeDao{
 		return sqlSession.selectOne("notice.count", param);
 	}
 	
+	//시퀀스 번호 생성
 	@Override
 	public int getSequenceNumber() {
 		return sqlSession.selectOne("notice.seq");
 	}
 
+	//글 수정
 	@Override
 	public void edit(NoticeDto noticeDto) {
 		sqlSession.update("notice.edit", noticeDto);		
 	}
-
-
+	
 }
