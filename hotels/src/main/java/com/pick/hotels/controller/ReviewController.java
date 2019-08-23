61938463974927fc5cf8c9f24079d0c0bec507a5
sohyun <huny6226@nate.com>
2019-08-23 19:01:49 +0900

    member
    미완성 집가장

--- hotels/src/main/java/com/pick/hotels/controller/ReviewController.java
+++ hotels/src/main/java/com/pick/hotels/controller/ReviewController.java
@@ -1,10 +1,17 @@
 package com.pick.hotels.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.pick.hotels.entity.ReviewDto;
 import com.pick.hotels.repository.ReviewDao;
 
 @Controller
@@ -13,6 +20,23 @@ public class ReviewController {
 	
 	@Autowired
 	private ReviewDao reviewDao;
+	
+	@GetMapping("/write")
+	public String write() {
+		return "review/write";
+	}
+	
+	@PostMapping("/write")
+	public String write(@ModelAttribute ReviewDto reviewDto, 
+						HttpSession session,
+						Model model) {
+		int member_no = (int) session.getAttribute("ok");
+		reviewDto.setReview_name(member_no);
+		
+		boolean result = reviewDao.write(reviewDto);
+		
+		return "redirect:content";
+	}
 	
 	
 	
