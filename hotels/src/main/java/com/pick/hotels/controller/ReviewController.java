d5ef42c223f6d8bb255074ccee0436d6ebdd3e98
sohyun <huny6226@nate.com>
2019-08-23 17:31:13 +0900

    기본내용

--- hotels/src/main/java/com/pick/hotels/controller/ReviewController.java
+++ hotels/src/main/java/com/pick/hotels/controller/ReviewController.java
@@ -1,10 +1,21 @@
 package com.pick.hotels.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pick.hotels.repository.ReviewDao;
 
 @Controller
 @RequestMapping("/review")
 public class ReviewController {
-
+	
+	@Autowired
+	private ReviewDao reviewDao;
+	
+	
+	
+	
+	
 }
