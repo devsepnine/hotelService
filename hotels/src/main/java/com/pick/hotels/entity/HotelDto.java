134de775e8ff373aad7b6daa98be662a9aec25a8
woogeun <xoommnj@gmail.com>
2019-08-27 15:38:02 +0900

    호텔 레지스트

--- hotels/src/main/java/com/pick/hotels/entity/HotelDto.java
+++ hotels/src/main/java/com/pick/hotels/entity/HotelDto.java
@@ -8,9 +8,10 @@ import lombok.NoArgsConstructor;
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class HotelDto {
 	private String hotel_name, hotel_zip_code, hotel_basic_addr, hotel_detail_addr, 
-	hotel_content, hotel_parking, hotel_ballet, hotel_fitness, hotel_Storage, 
-	hotel_pool, hotel_sauna, hotel_lounge, hotel_bbq, hotel_cafe, hotel_tel,
-	hotel_convenience_store, hotel_karaoke, hotel_internet, seller_id;
+	hotel_content, hotel_parking, hotel_fitness, hotel_pool, hotel_sauna, 
+	hotel_lounge, hotel_bbq, hotel_cafe, hotel_tel,hotel_convenience_store, 
+	hotel_karaoke, hotel_internet, seller_id;
+	
 	private int hotel_no, hotel_longitude, hotel_latitude, hotel_star, 
 	region_no;
 }
