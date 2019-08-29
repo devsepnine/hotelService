86b77b3614ca98d701d5829eeed6681ccc1cc52e
KIM <devsepnine@hotmail.com>
2019-08-29 18:12:42 +0900

    호텔 리스트
    평점
    아이콘
    기본 틀 제작

--- hotels/src/main/java/com/pick/hotels/entity/HotelListVo.java
+++ hotels/src/main/java/com/pick/hotels/entity/HotelListVo.java
@@ -8,8 +8,19 @@ import lombok.NoArgsConstructor;
 @Data @Builder @AllArgsConstructor @NoArgsConstructor
 public class HotelListVo {
 
-	private int hotel_no, room_cnt, max_price;
+	private int hotel_no, room_cnt, max_price, hotel_star;
 	
-	private String hotel_name, hotel_tel, hotel_zip_code, hotel_basic_addr, hotel_title,
-	hotel_star,hotel_parking, hotel_fitness,hotel_pool,hotel_sauna,hotel_lounge,hotel_bbq,hotel_cafe,hotel_convenience_store,hotel_karaoke,hotel_internet;
+	private String hotel_title, //썸네일
+					hotel_name, //호텔 이름
+					hotel_basic_addr, //호텔 주소
+					hotel_parking, //주차장 여부
+					hotel_fitness, //피트니스 여부
+					hotel_pool,  //수영장 여부
+					hotel_sauna, //사우나 여부
+					hotel_lounge, //라운지 여부
+					hotel_bbq, //바베큐 가능?
+					hotel_cafe, //카페 여부
+					hotel_convenience_store, //편의점 여부
+					hotel_karaoke, // 노래방 여부
+					hotel_internet; //인터넷 여부
 }
