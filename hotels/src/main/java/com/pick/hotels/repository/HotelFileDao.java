0a67c3de53c55e53af1bab6e73b9579c215ec4ae
woogeun <xoommnj@gmail.com>
2019-08-30 21:03:38 +0900

    호텔 수정 post맵핑 뺴고 완료

--- hotels/src/main/java/com/pick/hotels/repository/HotelFileDao.java
+++ hotels/src/main/java/com/pick/hotels/repository/HotelFileDao.java
@@ -1,9 +1,13 @@
 package com.pick.hotels.repository;
 
+import java.util.List;
+
 import com.pick.hotels.entity.HotelFileDto;
 
 public interface HotelFileDao {
 
 	void regist(HotelFileDto hfdto);
+
+	List<HotelFileDto> getlist(int hotel_no);
 
 }
